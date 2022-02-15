80857433fa87eae364529a45f186f8185fee9fd6
John Jones <jmjatlanta@gmail.com>
2022-02-15 09:44:20 -0500

    consolidate network differences

---
+++ src/komodo_genesis.h
@@ -0,0 +1,25 @@
+#pragma once
+/******************************************************************************
+ * Copyright © 2022 The Komodo Project Developers.                            *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * SuperNET software, including this file may be copied, modified, propagated *
+ * or distributed except according to the terms contained in the LICENSE file *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+/***
+ * This file is different between chains (i.e. mainnet and testnet) on purpose. 
+ * Merging between branches should be done with care.
+ */
+
+const char *Notaries_genesis[][2] =
+{
+    { "jmj_testA",   "037c032430fd231b5797cb4a637dae3eadf87b10274fd84be31670bd2a02c4fbc5" }
+};
